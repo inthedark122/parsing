b86b1b3673c12ba906481da87bedc9b100795feb
inthedark <padshyangel@mail.ru>
2014-08-21 00:18:36 +0300

    desc

--- app/controllers/churches_controller.rb
+++ app/controllers/churches_controller.rb
@@ -4,7 +4,7 @@ class ChurchesController < ApplicationController
     if params[:type]
       @churches = Church.where(:type_cherch => params[:type])
     else
-      @churches = Church.all
+      @churches = Church.all.order("type_church DESC")
     end
     respond_to do |format|
       format.html { render 'churches/show' }
